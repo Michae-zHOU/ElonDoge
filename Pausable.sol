5450942919815b2ebc5b66a2c805ba113380cf98
Michael Zhou <michael.zhouzzy@gmail.com>
2022-08-31 22:24:57 -0700

    [ERC20] Submit initial token contract.

---
+++ Pausable.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "Ownable.sol";
+
+contract Pausable is Ownable {
+
+    bool private _paused;
+
+    event Paused(address account);
+    event Unpaused(address account);
+
+    modifier whenNotPaused() {
+        require(!_paused, "Pausable: paused");
+        _;
+    }
+
+    modifier whenPaused() {
+        require(_paused, "Pausable: not paused");
+        _;
+    }
+
+    function paused() external view returns (bool) {
+        return _paused;
+    }
+
+    function pause() external onlyOwner whenNotPaused {
+        _paused = true;
+        emit Paused(msg.sender);
+    }
+
+    function unpause() external onlyOwner whenPaused {
+        _paused = false;
+        emit Unpaused(msg.sender);
+    }
+}
